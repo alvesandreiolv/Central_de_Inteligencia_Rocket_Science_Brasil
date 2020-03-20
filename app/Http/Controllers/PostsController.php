e91a9d28db504f3bbcf96c7f23cf40fdc663f41c
Neyelson Alves <neyelson@gmail.com>
2020-03-20 04:47:50 -0300

    Implementando o sistema de criar posts 2.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -48,7 +48,7 @@ class PostsController extends Controller
 		// $value->post_author = DB::connection('mysql')->table('users')->select('name')->where('rsbwordpressid', $value->post_author)->value('name');
 			
 			if (isset($users[$value->post_author])) {
-			$value->post_author = $users[$value->post_author];
+				$value->post_author = $users[$value->post_author];
 			}
 
 			if ($value->post_status === 'publish') {
@@ -96,6 +96,124 @@ class PostsController extends Controller
 
 	}
 
-	
+	public function criarpost(Request $dados) {
+
+		$validator = Validator::make($dados->all(), [
+
+			'titulo' => 'required|min:10|max:100',
+			'categoria' => 'required|in:2,4,267',
+			'imagem_destaque' => 'required|mimes:jpeg,jpg|dimensions:width=800,height=400',
+			'textocompleto' => 'required|min:100|max:1000000',
+
+		]);
+
+		if ($validator->fails()) {
+			return view('posts/criarpost', compact('dados'))->withErrors($validator);
+		}
+
+		$dados = $dados->all();
+		//$dados['rsbwordpressid']
+
+		$ID = DB::connection('mysql2')->table('wp_rocketsciencebrposts')->max('ID');
+		$ID++;
+
+		$replace = [
+			'&lt;' => '', '&gt;' => '', '&#039;' => '', '&amp;' => '',
+			'&quot;' => '', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä'=> 'Ae',
+			'&Auml;' => 'A', 'Å' => 'A', 'Ā' => 'A', 'Ą' => 'A', 'Ă' => 'A', 'Æ' => 'Ae',
+			'Ç' => 'C', 'Ć' => 'C', 'Č' => 'C', 'Ĉ' => 'C', 'Ċ' => 'C', 'Ď' => 'D', 'Đ' => 'D',
+			'Ð' => 'D', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ē' => 'E',
+			'Ę' => 'E', 'Ě' => 'E', 'Ĕ' => 'E', 'Ė' => 'E', 'Ĝ' => 'G', 'Ğ' => 'G',
+			'Ġ' => 'G', 'Ģ' => 'G', 'Ĥ' => 'H', 'Ħ' => 'H', 'Ì' => 'I', 'Í' => 'I',
+			'Î' => 'I', 'Ï' => 'I', 'Ī' => 'I', 'Ĩ' => 'I', 'Ĭ' => 'I', 'Į' => 'I',
+			'İ' => 'I', 'Ĳ' => 'IJ', 'Ĵ' => 'J', 'Ķ' => 'K', 'Ł' => 'K', 'Ľ' => 'K',
+			'Ĺ' => 'K', 'Ļ' => 'K', 'Ŀ' => 'K', 'Ñ' => 'N', 'Ń' => 'N', 'Ň' => 'N',
+			'Ņ' => 'N', 'Ŋ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O',
+			'Ö' => 'Oe', '&Ouml;' => 'Oe', 'Ø' => 'O', 'Ō' => 'O', 'Ő' => 'O', 'Ŏ' => 'O',
+			'Œ' => 'OE', 'Ŕ' => 'R', 'Ř' => 'R', 'Ŗ' => 'R', 'Ś' => 'S', 'Š' => 'S',
+			'Ş' => 'S', 'Ŝ' => 'S', 'Ș' => 'S', 'Ť' => 'T', 'Ţ' => 'T', 'Ŧ' => 'T',
+			'Ț' => 'T', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'Ue', 'Ū' => 'U',
+			'&Uuml;' => 'Ue', 'Ů' => 'U', 'Ű' => 'U', 'Ŭ' => 'U', 'Ũ' => 'U', 'Ų' => 'U',
+			'Ŵ' => 'W', 'Ý' => 'Y', 'Ŷ' => 'Y', 'Ÿ' => 'Y', 'Ź' => 'Z', 'Ž' => 'Z',
+			'Ż' => 'Z', 'Þ' => 'T', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a',
+			'ä' => 'ae', '&auml;' => 'ae', 'å' => 'a', 'ā' => 'a', 'ą' => 'a', 'ă' => 'a',
+			'æ' => 'ae', 'ç' => 'c', 'ć' => 'c', 'č' => 'c', 'ĉ' => 'c', 'ċ' => 'c',
+			'ď' => 'd', 'đ' => 'd', 'ð' => 'd', 'è' => 'e', 'é' => 'e', 'ê' => 'e',
+			'ë' => 'e', 'ē' => 'e', 'ę' => 'e', 'ě' => 'e', 'ĕ' => 'e', 'ė' => 'e',
+			'ƒ' => 'f', 'ĝ' => 'g', 'ğ' => 'g', 'ġ' => 'g', 'ģ' => 'g', 'ĥ' => 'h',
+			'ħ' => 'h', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ī' => 'i',
+			'ĩ' => 'i', 'ĭ' => 'i', 'į' => 'i', 'ı' => 'i', 'ĳ' => 'ij', 'ĵ' => 'j',
+			'ķ' => 'k', 'ĸ' => 'k', 'ł' => 'l', 'ľ' => 'l', 'ĺ' => 'l', 'ļ' => 'l',
+			'ŀ' => 'l', 'ñ' => 'n', 'ń' => 'n', 'ň' => 'n', 'ņ' => 'n', 'ŉ' => 'n',
+			'ŋ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'oe',
+			'&ouml;' => 'oe', 'ø' => 'o', 'ō' => 'o', 'ő' => 'o', 'ŏ' => 'o', 'œ' => 'oe',
+			'ŕ' => 'r', 'ř' => 'r', 'ŗ' => 'r', 'š' => 's', 'ù' => 'u', 'ú' => 'u',
+			'û' => 'u', 'ü' => 'ue', 'ū' => 'u', '&uuml;' => 'ue', 'ů' => 'u', 'ű' => 'u',
+			'ŭ' => 'u', 'ũ' => 'u', 'ų' => 'u', 'ŵ' => 'w', 'ý' => 'y', 'ÿ' => 'y',
+			'ŷ' => 'y', 'ž' => 'z', 'ż' => 'z', 'ź' => 'z', 'þ' => 't', 'ß' => 'ss',
+			'ſ' => 'ss', 'ый' => 'iy', 'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G',
+			'Д' => 'D', 'Е' => 'E', 'Ё' => 'YO', 'Ж' => 'ZH', 'З' => 'Z', 'И' => 'I',
+			'Й' => 'Y', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+			'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F',
+			'Х' => 'H', 'Ц' => 'C', 'Ч' => 'CH', 'Ш' => 'SH', 'Щ' => 'SCH', 'Ъ' => '',
+			'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'YU', 'Я' => 'YA', 'а' => 'a',
+			'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo',
+			'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l',
+			'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's',
+			'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch',
+			'ш' => 'sh', 'щ' => 'sch', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e',
+			'ю' => 'yu', 'я' => 'ya'
+		];
+
+		//ISSO TUDO ABAIXO É PARA FAZER O TRATAMENTO DO TITULO QUE SERÁ UTILIZADO TBM NA URL
+		$titulotratado = strtr($dados['titulo'], $replace);
+		$titulotratado = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $titulotratado))); //substitui tudo que não serve para uma url
+		$ultimocharacter = substr($titulotratado, -1); //pega o ultimo caractere da string
+		if ($ultimocharacter === '-') { //caso o ultimo caractere for -, remova ele. Isso serve pq o wordpress não reconhece links que terminam com esse caracere
+			$titulotratado = substr_replace($titulotratado, "", -1);
+		}
+
+		DB::connection('mysql2')
+		->table('wp_rocketsciencebrposts')
+		->insert(
+			[
+				'ID' => $ID,
+				'post_author' => Auth::id(),
+				'post_date' => Carbon::now(),
+				'post_date_gmt' => Carbon::now(),
+				'post_content' => $dados['textocompleto'],
+				'post_title' => $dados['titulo'],
+				'post_excerpt' => '',
+				'comment_status' => 'open',
+				'ping_status' => 'open',
+				'post_password' => '',
+				'post_name' => $ID.'-'.$titulotratado,
+				'to_ping' => '',
+				'pinged' => '',
+				'post_modified' => Carbon::now(),
+				'post_modified_gmt' => Carbon::now(),
+				'post_content_filtered' => '',	
+				'post_parent' => 0,	
+				'guid' => 'https://rocketsciencebr.com/?p='.$ID,	
+				'menu_order' => 0,				
+				'post_type' => 'post',	
+				'post_mime_type' => '',	
+				'comment_count' => 0,	
+			]
+		);
+
+		DB::connection('mysql2')
+		->table('wp_rocketsciencebrterm_relationships')
+		->insert(
+			[
+				'object_id' => $ID,
+				'term_taxonomy_id' => $dados['categoria'],
+				'term_order' => 0,
+			]
+		);
+
+		return redirect()->route('posts')->with('mensagemSucesso', 'O post "'.$dados['titulo'].'" foi publicado com sucesso.');
+
+	}
 
 }
--- resources/views/posts/criarpost.blade.php
+++ resources/views/posts/criarpost.blade.php
@@ -10,16 +10,28 @@
 
 	$(document).ready(function(){
 
-	//SCRIPT ABAIXO SERVE PARA UPLOAD DE IMAGENS
-	document.getElementById("imagem").onchange = function () {
-		var reader = new FileReader();
-		reader.onload = function (e) {
-			document.getElementById("imgpreview").src = e.target.result;
-		};
-		reader.readAsDataURL(this.files[0]);
-	};
+		//SCRIPT ABAIXO SERVE PARA O EDITOR DE TEXTO
+		tinymce.init({
+			selector: '#textocompleto',
+			plugins: "image",
+			menubar: 'favs file edit view insert format tools table help',
+			image_uploadtab: true,
+			paste_data_images : true,
+			images_upload_handler: function (blobInfo, success, failure) {
+				success("data:" + blobInfo.blob().type + ";base64," + blobInfo.base64());
+			}
+		});
 
-});
+		//SCRIPT ABAIXO SERVE PARA UPLOAD DE IMAGENS
+		document.getElementById("imagem_destaque").onchange = function () {
+			var reader = new FileReader();
+			reader.onload = function (e) {
+				document.getElementById("imgpreview").src = e.target.result;
+			};
+			reader.readAsDataURL(this.files[0]);
+		};
+
+	});
 
 </script>
 
@@ -36,7 +48,7 @@
 <div class="card bg-aeroblack mt-2">
 	<div class="card-body">
 
-		<form style="margin-block-end: 0px !important;" method="post" action="{{url('/membros/editar')}}" enctype="multipart/form-data">
+		<form style="margin-block-end: 0px !important;" method="post" action="{{url('/posts/criarpost')}}" enctype="multipart/form-data">
 			{{ csrf_field() }}
 
 			<input type="hidden" id="id" name="id" value="asd">
@@ -47,42 +59,52 @@
 
 			<br><br>
 
-			<small><i class="text-warning fa fa-exclamation-circle" aria-hidden="true"></i> A imagem destaque deve seguir rigorosamente o formato: JPG com 800 pixels de Largura e 400 pixels de altura.</small>
+			<small><i class="text-warning fa fa-exclamation-circle" aria-hidden="true"></i> A imagem destaque deve seguir rigorosamente o padrão: JPG ou JPEG com 800 pixels de Largura e 400 pixels de altura.</small>
 
 			<hr>
 
 			<div class="form-group">
 				<div class="form-row">
-
 					<div class="col">
-						<label for="name"><i class="text-primary fa fa-globe" aria-hidden="true"></i> Título do post:</label>
-						<input type="text" name="name" required id="name" class="form-control bg-aeroblack" placeholder="">
+						<label for="titulo">Título do post:</label>
+						<input minlength="10" maxlength="100" type="text" name="titulo" required id="titulo" class="form-control bg-aeroblack" placeholder="" value="{{ $dados['titulo'] ?? '' }}">
 					</div>
-
 					<div class="col">
-						<label for="credencial">Categoria:</label>
-						<select class="custom-select bg-aeroblack" id="credencial" name="credencial">
-							<option value="0" selected>Notícias</option>
-							<option value="1">Reportagem</option>
-							<option value="2">Especial</option>
+						<label for="categoria">Categoria:</label>
+						<select class="custom-select bg-aeroblack" id="categoria" name="categoria">
+							<option value="4" <?php if (isset($dados['categoria'])) { if ($dados['categoria'] == 4) { echo 'selected'; } } else {	echo 'selected'; }  ?> >Notícias</option>
+							<option value="2" <?php if ((isset($dados['categoria'])) && ($dados['categoria'] == 2)) { echo 'selected'; } ?> >Reportagem</option>
+							<option value="267" <?php if ((isset($dados['categoria'])) && ($dados['categoria'] == 267)) { echo 'selected'; } ?> >Especial</option>
 						</select>
 					</div>	
-
 				</div>
 			</div>
 
-			<div class="form-row">
-				<div class="col">
-					<img id="imgpreview" name="imgpreview" src="{{asset('public/images/assets/coverempty.jpg')}}" width="250" height="125" style="border-radius: 0.25rem; margin-right: 15px; float: left;" class="d-inline-block align-top" >
-					<i class="text-primary fa fa-globe" aria-hidden="true"></i> Imagem destaque:<br>
-					<small><input type="file" id="imagem" name="imagem" style="margin-top: 10px;" accept="image/*"  /></small><br>
-					<small><div style="margin-top: 10px;">Tipo: JPG. Largura: 800px. Altura: 400px.</div></small>
+			<div class="form-group">
+				<div class="form-row">
+					<div class="col">
+						<img id="imgpreview" name="imgpreview" src="{{asset('public/images/assets/coverempty.jpg')}}" width="250" height="125" style="border-radius: 0.25rem; margin-right: 15px; float: left;" class="d-inline-block align-top" >
+						Imagem destaque:<br>
+						<small><input type="file" id="imagem_destaque" name="imagem_destaque" style="margin-top: 10px;" accept=".jpg, .jpeg"  /></small><br>
+						<small><div style="margin-top: 10px;">Tipo: JPG ou JPEG. Largura: 800px. Altura: 400px.</div></small>
+					</div>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<div class="form-row">
+					<div class="col">
+						<label for="textocompleto">Texto completo:</label>
+						<textarea class="form-control bg-aeroblack" id="textocompleto" name="textocompleto" rows="15">{{ $dados['textocompleto'] ?? '' }}</textarea>
+					</div>
 				</div>
 			</div>
 
 			<hr>
 
-			<button type="submit" class="btn btn-success bg-aeroblack">Salvar alterações</button>
+			<button type="submit" class="btn btn-success bg-aeroblack">Publicar agora</button>
+			<!--<button type="submit" class="btn btn-info bg-aeroblack">Enviar para análise</button>-->
+			<button type="submit" class="btn btn-warning bg-aeroblack">Salvar como rascunho</button>
 
 		</form>
 
